4480dac30a5199af00b51884100e89ad8763e827
cbrunosps <91685392+cbrunosps@users.noreply.github.com>
2022-09-22 09:29:45 -0500

    fix: se elimina condición de script

--- scripts/crear-bucket-tfstate.py
+++ scripts/crear-bucket-tfstate.py
@@ -3,26 +3,26 @@ REGION = "us-west-2"
 sesion_origen = boto3.Session(region_name=REGION)
 s3_client = sesion_origen.client("s3")
 
-if not profile:
-    try:
-        print("Intentando crear bucket")
-        bucket_tfstate = "terraform-test-remote-state-sps"
 
-        response = s3_client.create_bucket(
-            Bucket=bucket_tfstate
-        )
-    except Exception as e:
-        print(e)
-        print("No fue posible crear bucket.")
+try:
+    print("Intentando crear bucket")
+    bucket_tfstate = "terraform-test-remote-state-sps"
 
-    try:
-        print("Habilitando versionado...")
-        response = s3_client.put_bucket_versioning(
-            Bucket=bucket_parametros,
-            VersioningConfiguration={
-                'Status': 'Enabled'
-            }
-        )
-    except Exception as e:
-        print(e)
-        print("No fue posible habilitar versionado")
+    response = s3_client.create_bucket(
+        Bucket=bucket_tfstate
+    )
+except Exception as e:
+    print(e)
+    print("No fue posible crear bucket.")
+
+try:
+    print("Habilitando versionado...")
+    response = s3_client.put_bucket_versioning(
+        Bucket=bucket_parametros,
+        VersioningConfiguration={
+            'Status': 'Enabled'
+        }
+    )
+except Exception as e:
+    print(e)
+    print("No fue posible habilitar versionado")

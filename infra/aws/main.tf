3a7021133bf7eb70438b3f032f9550e2c943ecb3
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-09-27 18:41:59 -0500

    feat: se agrega uso de módulo EKS de Terraform. Se elimina el ejemplo anterior

---
+++ infra/aws/main.tf
@@ -0,0 +1,129 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = var.cluster-name
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+#  cluster_encryption_config = [{
+#    provider_key_arn = "arn:aws:kms:eu-west-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+#    resources        = ["secrets"]
+#  }]
+
+  vpc_id     = aws_vpc.demo.id
+  subnet_ids = aws_subnet.demo[*].id
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "t2.micro"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = [
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    ]
+  }
+
+
+#  self_managed_node_groups = {
+#    one = {
+#      name         = "mixed-1"
+#      max_size     = 5
+#      desired_size = 1
+
+#      use_mixed_instances_policy = true
+#      mixed_instances_policy = {
+#        instances_distribution = {
+#          on_demand_base_capacity                  = 0
+#          on_demand_percentage_above_base_capacity = 10
+#          spot_allocation_strategy                 = "capacity-optimized"
+#        }
+#
+#        override = [
+#          {
+#            instance_type     = "t2.small"
+#            weighted_capacity = "1"
+#          },
+#          {
+#            instance_type     = "m6i.large"
+#            weighted_capacity = "2"
+#          },
+#        ]
+#      }
+#    }
+#  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    disk_size      = 50
+    instance_types = ["t2.small"]
+  }
+
+  eks_managed_node_groups = {
+    #blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t2.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+#  fargate_profiles = {
+#    default = {
+#      name = "default"
+#      selectors = [
+#        {
+#          namespace = "default"
+#        }
+#      ]
+#    }
+#  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::494098342225:role/github-role-skaffold-demo"
+      username = "github-role-skaffold-demo"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::494098342225:user/cbruno@spsolutions.com.mx"
+      username = "cbruno@spsolutions.com.mx"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::494098342225:user/rortega@spsolutions.com.mx"
+      username = "rortega@spsolutions.com.mx"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "494098342225",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
--- infra/aws/providers.tf
+++ infra/aws/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-tfstate-sps-demo"
-    key = "key/terraform.tfstate"
+    key = "tfstates/eks_demo_module.tfstate"
     region = "us-west-2"
   }
   required_version = ">= 0.12"
@@ -18,3 +18,22 @@ data "aws_availability_zones" "available" {}
 # to open EC2 Security Group access to the Kubernetes cluster.
 # See workstation-external-ip.tf for additional information.
 provider "http" {}
+
+/*
+The following 2 data resources are used get around the fact that we have to wait
+for the EKS cluster to be initialised before we can attempt to authenticate.
+*/
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}

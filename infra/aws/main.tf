38eeeefe7f7af4b7d5cffd302ae5fe3ef0f57b11
Carlos Bruno E <cbruno@spsolutions.com.mx>
2022-10-20 15:25:19 -0500

    infra: se reduce el clúster a un solo nodo pero con la capacidad de 2

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -73,11 +73,11 @@ module "eks" {
   eks_managed_node_groups = {
     #blue = {}
     green = {
-      min_size     = 2
+      min_size     = 1
       max_size     = 10
-      desired_size = 2
+      desired_size = 1
 
-      instance_types = ["t2.small"]
+      instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
     }
   }
